2660e54df9e3fe729e4703f3534a79f3384398cb
Micah Olivas <micah5051olivas@gmail.com>
2021-04-12 23:14:35 -0700

    Hashtable build complete

--- Miniproject_2/part_3.py
+++ Miniproject_2/part_3.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 import scipy.sparse as sparse
 import numpy as np
 import pandas as pd
-import itertools
+import importlib
 
 from part_1 import *
 
 ### part 3
-
 dat = pd.read_csv('Miniproject_2/data50.csv', sep = ',', names = ["articleId", "wordId", "Count"])
 groups = pd.read_csv('Miniproject_2/p2_data/groups.csv', names = ['Name'])
 labels = pd.read_csv('Miniproject_2/p2_data/label.csv', names = ['Label'])
@@ -16,14 +15,15 @@ labels = pd.read_csv('Miniproject_2/p2_data/label.csv', names = ['Label'])
 # Sparse matrix implementation is slow, so store full matrix for now
 word_mat = sparse.csr_matrix((dat.Count, (dat.articleId, dat.wordId))).todense()[1:,1:] # remove first row/col
 
-# Locality-Sensitive-Hashing
+## Locality-Sensitive-Hashing
 
 # Hyperplane Hashing
-def build_hashtables(d,k,l):
+def build_hashtables(v,d,k,l):
 	"""
 		Inputs:
-		-d:
-		-k: dimensionality of row vector in matrix
+		-v: a k-dimensional vector
+		-d: lentgth of ith hashtable matrix
+		-k: row dimension of ith hashtable matrix
 		-l: number of hashtables
 	"""
 	hashtables = []
@@ -34,18 +34,33 @@ def build_hashtables(d,k,l):
 		with np.nditer(classification_matrix, op_flags=['readwrite']) as it:
 		   for x in it:
 			   x[...] = np.random.normal(0,1)
-			   x[...] = np.sign(x)
 
 		hashtables.append(classification_matrix)
 
+	# hash given vector v
+	for tbl in range(l):
+		hashtables[tbl] = hashtables[tbl].dot(v)
+		with np.nditer(hashtables[tbl], op_flags=['readwrite']) as it:
+			for x in it:
+				if x > 0:
+					x[...] = 1
+				else:
+					x[...] = 0
+
 	return hashtables
 
-def classify_hashed(a):
-	"""
-		Inputs:
-		-q: query vector
+v = np.array([2,6,3,5,8,2,4,3,4,6])
 
-	"""
-
+build_hashtables(v,10,10,10)
 
 # Classification
+def classify_hashed(q):
+	"""
+		Inputs:
+		-q: a vector with which to query the l hashtables for similarity
+	"""
+
+
+# implementation
+l = 128
+d = range(5,21)
