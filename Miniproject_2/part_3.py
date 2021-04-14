016441d0e068692a9360f265528756c18fab35e8
Micah Olivas <micah5051olivas@gmail.com>
2021-04-13 17:47:03 -0700

    Update part_3.py

--- Miniproject_2/part_3.py
+++ Miniproject_2/part_3.py
@@ -4,10 +4,6 @@ import numpy as np
 import pandas as pd
 import importlib
 
-<<<<<<< HEAD
-from part_1 import *
-from part_2 import *
-=======
 # from part_1 import *
 
 # Previous functions
@@ -21,7 +17,6 @@ def cosine_similarity(a, b):
     """
     #return (a @ b.T)[0,0] / (sparse_norm(a)*sparse_norm(b))
     return (a @ b.T) / (np.linalg.norm(a)*np.linalg.norm(b))
->>>>>>> 6aa2d0e605f2b514a52c4c164c0add8b8522cf37
 
 ### part 3
 dat = pd.read_csv('Miniproject_2/data50.csv', sep = ',', names = ["articleId", "wordId", "Count"])
@@ -35,7 +30,7 @@ word_mat = sparse.csr_matrix((dat.Count, (dat.articleId, dat.wordId))).todense()
 ## Locality-Sensitive-Hashing
 
 # Construct hyperplane hashtables
-def build_hashtables(d,k,l):
+def build_empty_hashtables(d,k,l):
 	"""
 		Inputs:
 		-d: lentgth of ith hashtable matrix
@@ -55,14 +50,11 @@ def build_hashtables(d,k,l):
 
 	return matrix_array
 
-a = build_hashtables(5,5,5)
-a
-
 def hash_vector(v,matrix_array):
 	"""
 		Inputs:
 		-v: k-dimensional vector to hash
-		-matrix_array: array of matrices containing values randomly sampled from gaussian with u = 0, var = 1
+		-matrix_array: from build_empty_hashtables; array of matrices containing values randomly sampled from gaussian with u = 0, var = 1
 	"""
 	hashtables = matrix_array
 
@@ -78,10 +70,8 @@ def hash_vector(v,matrix_array):
 
 	return hashtables
 
-b = np.array([0,6,3,4,5])
-c = hash_vector(b,a)
-c
-a
+a = np.array([0,1,1,1,0])
+
 # Hash an array of vectors
 def hash_vectors(v_array,d,k,l):
 	"""
@@ -95,33 +85,6 @@ def hash_vectors(v_array,d,k,l):
 
 	return dict(zip(list(v_array), hashed_array))
 
-# Classification
-def brute_force_search(query, search_space, metric=cosine_similarity, self=0):
-    similarity_vec = np.apply_along_axis(lambda row: metric(row, query), 1, search_space)
-    similarity_vec[0,self] = np.NINF
-    max_idx = np.nanargmax(similarity_vec)
-    return max_idx
-
-def classify_query(q,d,k,l,tables):
-	"""
-		Inputs:
-		-q: a query vector
-		-tables: a set of hastables for classification
-	"""
-
-	# hash query data, assuming hashtables for
-	similar_obj = brute_force_search(q, tables)
-	sim_obj_label = labels.loc[sim_obj, 'Label']
-
-def build_classification_matrix(data_mat, groups=groups, labels=labels):
-    classification_matrix = np.zeros((len(groups),len(groups)))
-
-    for i in range(len(labels)):
-        true_label = labels.loc[i,'Label']
-        doc = data_mat[i]
-        most_sim_doc = brute_force_search(doc, data_mat, self=i)
-        most_sim_label = labels.loc[most_sim_doc,'Label']
-        classification_matrix[true_label-1,most_sim_label-1] += 1
 
     return classification_matrix
 
@@ -199,15 +162,17 @@ def compute_classification_accuracy(classification_mat):
     return classification_mat.diagonal().sum() / classification_mat.sum()
 
 class_accuracies = []
-
 for d_i in range(5,21):
 	hashtables_di = build_hashtables(d_i,)
 	class_di = classify_hashed()
 	class_accuracies.append(compute_classification_accuracy())
 
 plt.plot(range(5,21), class_accuracies, color='green', marker='o', linestyle='dashed', linewidth=2, markersize=12)
+		hashtables_empty_di, hashtables_d_i = build_hashtables(dat_mat, d_i, k, l)
+		class_di = build_classification_matrix()
+		class_accuracies.append(compute_classification_accuracy(hashtables_d_i, class_di))
 
-a = range(5,21)
-b = range(5,21)
+	errors_plot = plt.plot(range(5,21), class_accuracies, color='green', marker='o', linestyle='dashed', linewidth=2, markersize=12)
+	return errors_plot
 
-plt.plot(a, b, 'bo', linestyle='dashed', linewidth=2, markersize=8)
+##
