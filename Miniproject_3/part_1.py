77c1601ba05351d1bfd6b4b96d86b4631c6c797d
Micah Olivas <micah5051olivas@gmail.com>
2021-04-17 18:40:40 -0700

    Created Miniproject 3

--- Miniproject_3/part_1.py
+++ Miniproject_3/part_1.py
@@ -1,1 +1,11 @@
 import matplotlib as plt
+import numpy as np
+
+### Part 1
+
+## Regression
+d = 100 # dimensions of data
+n = 1000 # number of data points
+X = np.random.normal(0,1, size=(n,d))
+a_true = np.random.normal(0,1, size=(d,1))
+y = X.dot(a_true) + np.random.normal(0,0.5,size=(n,1))
