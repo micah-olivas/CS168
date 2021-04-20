8a3f9a6cc954d9546d5867cf6a246670fbdc4de7
bdoughty <benjamin.doughty@gmail.com>
2021-04-19 21:17:13 -0700

    finished part 2

--- Miniproject_3/part_2.py
+++ Miniproject_3/part_2.py
@@ -63,11 +63,8 @@ plt.savefig('figs/miniproject3.part2.b.png')
 plt.clf()
 
 # part c
-def error(X,a,y):
-    return np.square(np.linalg.norm(X.dot(a)-y))
-
 def error_gradient(X,a,y):
-    return 2*(X.dot(a)-y)*X
+    return (2*(X.dot(a)-y)*X).reshape(-1,1)
 
 def sgd(a_guess, X, y, alpha, n=train_n, n_iter=1000000):
     a_hat = a_guess
@@ -77,7 +74,7 @@ def sgd(a_guess, X, y, alpha, n=train_n, n_iter=1000000):
         y_i = y[idx]
         grad = error_gradient(x_i, a_hat, y_i)
         # before = error(x_i,a_hat,y_i)
-        a_hat -= alpha * grad
+        a_hat = a_hat - alpha * grad
         # after = error(x_i,a_hat,y_i)
         # assert before > after
     return a_hat
@@ -89,14 +86,14 @@ data = []
 for alpha in alphas:
     for _ in range(n_trials):
         X_train, a_true, y_train, X_test, y_test = generate_data()
-        a_guess = np.zeros(d)
-        a_hat = sgd(a_guess, X_train, y_train, alpha, n=train_n)
+        a_guess = np.zeros((d,1))
+        a_hat = sgd(a_guess, X_train, y_train, alpha)
         train_err, test_err = normalized_error(X_train,a_hat,y_train), normalized_error(X_test,a_hat,y_test)
-        # train_err_true, test_err_true = normalized_error(X_train,a_true,y_train), normalized_error(X_test,a_true,y_test)
+        train_err_true, test_err_true = normalized_error(X_train,a_true,y_train), normalized_error(X_test,a_true,y_test)
         data.append({'alpha':alpha,'Error':train_err,'Error_type':'train'})
         data.append({'alpha':alpha,'Error':test_err,'Error_type':'test'})
-#         data.append({'alpha':alpha,'Error':train_err_true,'Error_type':'train_true'})
-#         data.append({'alpha':alpha,'Error':test_err_true,'Error_type':'test_true'})
+        data.append({'alpha':alpha,'Error':train_err_true,'Error_type':'train_true'})
+        data.append({'alpha':alpha,'Error':test_err_true,'Error_type':'test_true'})
 
 df = pd.DataFrame(data)
 sns.barplot(data=df,x='alpha',y='Error',hue='Error_type')
@@ -104,5 +101,70 @@ plt.savefig('figs/miniproject3.part2.c.png')
 plt.clf()
 
 # part d
+def augmented_sgd(a_guess, X, y, alpha, n=train_n, n_iter=1000000, sampling_rate=100):
+    a_hat = a_guess
+    a_arr = np.zeros((a_guess.size, int(n_iter/sampling_rate)))
+    for i in range(n_iter):
+        idx = np.random.randint(n)
+        x_i = X[idx]
+        y_i = y[idx]
+        grad = error_gradient(x_i, a_hat, y_i)
+        a_hat = a_hat - alpha * grad
+        if i % sampling_rate == 0:
+            a_arr[:,int(i/sampling_rate)] = a_hat.reshape(-1)
+    return a_hat, a_arr
+
+alphas = [0.00005, 0.005]
+data = []
+sr = 100
+n_iter = 1000000
+
+fig, axs = plt.subplots(3,2, figsize=(7,7))
+
+for idx, alpha in enumerate(alphas):
+    X_train, a_true, y_train, X_test, y_test = generate_data()
+    a_guess = np.zeros((d,1))
+    a_hat, a_arr = augmented_sgd(a_guess, X_train, y_train, alpha, n_iter=n_iter, sampling_rate=sr)
+    axs[0,idx].plot(range(n_iter//sr),[normalized_error(X_train,a_arr[:,i].reshape(-1,1),y_train) for i in range(n_iter//sr)])
+    axs[0,idx].axhline(normalized_error(X_train,a_true,y_train), ls='--')
+    if idx == 0:
+        axs[0,idx].set_ylabel('Normalized training error')
+    axs[0,idx].set_title(alpha)
+    axs[1,idx].plot(range(n_iter//sr),[normalized_error(X_test,a_arr[:,i].reshape(-1,1),y_test) for i in range(n_iter//sr)])
+    axs[1,idx].axhline(normalized_error(X_test,a_true,y_test), ls='--')
+    if idx == 0:
+        axs[1,idx].set_ylabel('Normalized test error')
+    axs[2,idx].plot(range(n_iter//sr),[np.linalg.norm(a_arr[:,i].reshape(-1,1)) for i in range(n_iter//sr)])
+    axs[2,idx].set_xlabel('Iteration (x {})'.format(sr))
+    if idx == 0:
+        axs[2,idx].set_ylabel('Norm(a)')
+
+plt.savefig('figs/miniproject3.part2.d.png')
+plt.clf()
+
+# part e
+def sample_from_unit_ball(r, d=d):
+    v = np.random.normal(0,1,size=(d,1))
+    return v / np.linalg.norm(v) * r
+
+alpha = 0.00005
+rs = [0, 0.1, 0.5, 1, 10, 20, 30]
+n_trials = 10
+
+data = []
+
+for r in rs:
+    for _ in range(n_trials):
+        X_train, a_true, y_train, X_test, y_test = generate_data()
+        a_guess = sample_from_unit_ball(r)
+        a_hat = sgd(a_guess, X_train, y_train, alpha)
+        train_err, test_err = normalized_error(X_train,a_hat,y_train), normalized_error(X_test,a_hat,y_test)
+        data.append({'r':r,'Error':train_err,'Error_type':'train'})
+        data.append({'r':r,'Error':test_err,'Error_type':'test'})
+
+df = pd.DataFrame(data)
+sns.barplot(data=df,x='r',y='Error',hue='Error_type')
+plt.savefig('figs/miniproject3.part2.e.png')
+plt.clf()
 
 
