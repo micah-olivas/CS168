Added plot for part 1b; corrected gd formula

Miniproject_3/part_1.py
@@ -37,17 +37,34 @@ print(y_hat, y_zeroes)
 def get_gradient(X,a,y):
     return (2*(X.dot(a)-y)*X).reshape(-1,1)
 
-def descend_gradient(a_guess, X, y, step_size, n_train, n_iter=1000):
-    a_hat = a_guess
-    for i in range(n_iter):
+def error(X,a,y):
+    return np.square(np.linalg.norm(X.dot(a)-y))
 
+def descend_gradient(a_guess, X, y, step_size, n_train, n_iter=20):
+    a_hat = a_guess
+    iter_number = []
+    obj_fun_values = []
+    for i in range(n_train):
         x_i = X[i]
         y_i = y[i]
         a_hat = a_hat - (step_size * get_gradient(x_i, a_hat, y_i))
-    return a_hat
+        iter_number += [i]
+        obj_fun_values += [error(x_i, a_hat, y_i)]
+    return a_hat, iter_number, obj_fun_values
+
+a_hat_gd_00005, iter_numbers_00005, obj_fun_values_00005 = descend_gradient(a_hat, X, y, 0.00005, 100, 20)
+a_hat_gd_0005, iter_numbers_0005, obj_fun_values_0005 = descend_gradient(a_hat, X, y, 0.0005, 100, 20)
+a_hat_gd_0007, iter_numbers_0007, obj_fun_values_0007 = descend_gradient(a_hat, X, y, 0.0007, 100, 20)
 
 
-step_sizes = [0.00005, 0.0005, 0.0007]
+plt.plot(iter_numbers_00005, obj_fun_values_00005, 'ro')
+plt.plot(iter_numbers_0005, obj_fun_values_0005, 'mo')
+plt.plot(iter_numbers_0007, obj_fun_values_0007, 'bo')
+plt.xlabel('Iteration')
+plt.ylabel('Objective Function Value (Error)')
+plt.savefig('figs/part1b.gd_all.png')
+plt.clf()
+
 
 # 1.c stochastic gradient descent using ben's method
 train_n = 100