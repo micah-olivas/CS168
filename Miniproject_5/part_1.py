81a7e3ec190b8334558678d24ff2b76d5c4f488e
Micah Olivas <micah5051olivas@gmail.com>
2021-04-29 10:04:41 -0700

    Update part_1.py

--- Miniproject_5/part_1.py
+++ Miniproject_5/part_1.py
@@ -6,20 +6,22 @@ import math
 # 1.a Confirming processing
 co_occur = pd.read_csv('Miniproject_5/co_occur.csv', sep = ',', names = list(range(10000)))
 
-## 1.b Normalize matrix and compute rank-100 approx
+# 1.b Normalize matrix and compute rank-100 approx
 def norm_matrix(elem):
     elem = math.log(elem + 1)
     return elem
 
 norm_co_occur = co_occur.applymap(norm_matrix)
-top_co_occur = norm_co_occur.iloc[:100, :100]
+top_co_occur = norm_co_occur.iloc[:3000, :3000]
 
 # Single Value Decomposition
-u, s, vh = np.linalg.svd(norm_co_occur)
+u, s, vh = np.linalg.svd(top_co_occur)
 s
 
-W = top_co_occur @ np.transpose(top_co_occur)
-W
+# Plot singular values
+plt.scatter(y = s, x = list(range(len(s))))
+plt.ylabel('Singular Value')
+plt.savefig('Miniproject_5/mp5-part1b.png', dpi = 300)
 
-# Plot single values
-plt.stem(s)
+# 1.c
+u
