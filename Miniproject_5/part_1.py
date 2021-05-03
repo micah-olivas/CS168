cf7fed5ebda5d40cbeb13a1b752a404b9423e73b
Micah Olivas <micah5051olivas@gmail.com>
2021-05-03 09:52:22 -0700

    Updates

--- Miniproject_5/part_1.py
+++ Miniproject_5/part_1.py
@@ -3,8 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-# 1.a Confirming processing
+# 1.a Confirming co-occur, dictionary processing
 co_occur = pd.read_csv('Miniproject_5/co_occur.csv', sep = ',', names = list(range(10000)))
+with open('Miniproject_5/dictionary.txt') as f:
+    content = f.readlines()
+dict = [x.strip() for x in content]
 
 # 1.b Normalize matrix and compute rank-100 approx
 def norm_matrix(elem):
@@ -21,7 +24,49 @@ s
 # Plot singular values
 plt.scatter(y = s, x = list(range(len(s))))
 plt.ylabel('Singular Value')
+plt.show()
 plt.savefig('Miniproject_5/mp5-part1b.png', dpi = 300)
 
-# 1.c
-u
+# 1.c index single
+
+# Get co-occurrence values from word pair
+def words_co_occurence(word1, word2):
+    """
+        inputs:
+            -word1 : first word
+            -word2 : second word
+        returns:
+            score from the co-occurrence matrix corresponding
+            to the intersection of word1 and word2
+    """
+    word1 = str(word1)
+    word2 = str(word2)
+
+    idx1 = dict.index(word1)
+    idx2 = dict.index(word2)
+
+    co_occurrence = norm_co_occur.iloc[idx1,idx2]
+
+    return co_occurrence
+
+words_co_occurence("chicago", "sacramento")
+
+# 1.d.i
+v_words = ["boy", "girl", "brother", "sister", "king", "queen", he, she, john, mary, wall, tree]
+
+# 1.e.i
+def cosine_similarity(a, b):
+    """
+        inputs:
+            -a : a csr sparse vector
+            -b : another csr sparse vector
+        returns:
+            the cosine similarity of the two sparse vectors
+    """
+    #return (a @ b.T)[0,0] / (sparse_norm(a)*sparse_norm(b))
+    return (a @ b.T) / (np.linalg.norm(a)*np.linalg.norm(b))
+
+
+vec1 = [1,2,3,4,5]
+vec2 = [2,3,4,5,6]
+vec3 = [9,9,9,9,9]
